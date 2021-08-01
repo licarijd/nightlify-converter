b2e543748ad994c7b381f71c4dec0afce5ec06be
Justin Licari <justinlicari@Justins-MacBook-Air.local>
2021-08-01 13:27:07 -0400

    Use s3 stored image for integration tests

--- integration-tests/image.test.js
+++ integration-tests/image.test.js
@@ -3,7 +3,7 @@ import Jimp from 'jimp'
 
 describe('Write image to file', () => {
     it('should convert an image to a nightmode friendly image and write it to a file', (done) => {
-        const imageUrl = './integration-tests/data/smwmelephants3-4.png'
+        const imageUrl = 'https://original-uploaded-images-dev.s3.ca-central-1.amazonaws.com/dev-1627756609448-images.jpeg'
         const newImageUrl = './integration-tests/data/newImage.png'
         const sampleNewImageUrl = './integration-tests/data/testImage.png'
         

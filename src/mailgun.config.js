111f71d8f556e88040c4f349a58f8d040a24e765
Justin Licari <licarijd@users.noreply.github.com>
2021-08-02 23:21:47 -0400

    Delete mailgun.config.js

--- src/mailgun.config.js
+++
@@ -1,3 +0,0 @@
-export const DOMAIN = "sandbox2c981a28f6744b2ba774ac1209d5d841.mailgun.org"
-export const API_KEY = "56d7d298a9dd448aece950d977134704-64574a68-3f7492e5"
-export const SENDER = "Mailgun Sandbox <postmaster@sandbox2c981a28f6744b2ba774ac1209d5d841.mailgun.org>"

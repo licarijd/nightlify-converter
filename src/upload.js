934186ae6d2a84a827f44dd0ccaf40fac48b6f89
Justin Licari <justinlicari@Justins-MacBook-Air.local>
2021-08-02 21:16:02 -0400

    Remove unused code

--- src/upload.js
+++ src/upload.js
@@ -1,10 +1,9 @@
-import S3 from 'aws-s3';
 import AWS from 'aws-sdk'
 import { keys } from './s3AccessKeys';
 import { config } from './s3.config'
 
-const S3UploadClient = new S3(config)
 const S3DownloadClient = new AWS.S3()
+
 S3DownloadClient.config.update({
     bucketName: 'original-uploaded-images-dev',
     region: config.region,
@@ -12,38 +11,13 @@ S3DownloadClient.config.update({
     secretAccessKey: keys.secretAccessKey,
 })
 
-export const uploadToS3 = async (file, fileName) => {
-    return await S3UploadClient
-        .uploadFile(file, fileName)
-        .then(data => {
-            console.log(data)
-            return data
-        })
-        .catch(err => {
-            console.error(err)
-            return err
-        })
-    
-    /**
-     * {
-     *   Response: {
-     *     bucket: "your-bucket-name",
-     *     key: "photos/image.jpg",
-     *     location: "https://your-bucket.s3.amazonaws.com/photos/image.jpg"
-     *   }
-     * }
-     */
-}
-
-
-
 export const uploadConvertedFileToS3 = (file, key) => {
     const params = {
         Bucket: config.bucketName,
         Key: key,
         Body: file,
-        //ContentType: image.mimetype,
         ACL: 'public-read'
+        //ContentType: image.mimetype
       };
 
       S3DownloadClient.upload(params, async (err, data) => {
@@ -65,7 +39,7 @@ export const getFileFromS3 = async (key, bucket) => {
     
         const data = await S3DownloadClient.getObject(params).promise();
     
-        return data.Body//.toString('utf-8');
+        return data.Body
     } catch (e) {
         throw new Error(`Could not retrieve file from S3: ${e.message}`)
     }

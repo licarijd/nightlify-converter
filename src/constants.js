e00e5a8cfc164be461a057306f3f611bcdf2bbde
Justin Licari <justinlicari@Justins-MacBook-Air.local>
2021-08-13 23:52:21 -0400

    S3 constants

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,4 @@
 export const CONVERTED_IMAGES_BUCKET = 'converted-images-dev'
+export const ORIGINAL_IMAGES_BUCKET = 'original-uploaded-images-dev'
+export const WATERMARK_IMAGE_BUCKET = 'conversion-assets'
+export const WATERMARK_IMAGE_KEY = 'addtext_com_MTkxOTM1MTc1MjI.png'
